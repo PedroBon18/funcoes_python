b060e5594d1bc9b7b7be15d6f4e36626354d25df
PedroBon18 <pedrobondev18@gmail.com>
2026-05-27 19:21:58 -0300

    V0.0.2: exe02

---
+++ exe02.py
@@ -0,0 +1,10 @@
+def somar(a, b):
+    somar = a + b
+    print (somar)
+
+somar (42,616)
+
+# No universo de O Guia do Mochileiro das Galáxias, 42 é a "Resposta para a Pergunta Fundamental sobre a Vida, o Universo e Tudo Mais". 
+# Ela foi calculada ao longo de 7,5 milhões de anos pelo supercomputador Pensador Profundo (Deep Thought)
+
+# É o nome do universo principal das histórias em quadrinhos da editora Marvel (Terra-616).
